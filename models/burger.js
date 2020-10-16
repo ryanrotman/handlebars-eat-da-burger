0061565d5af9247347402afe9b943eac138a6016
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-15 22:43:50 -0500

    added burger.js model code

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+    selectAll(cb) {
+        orm.selectAll("burgers", (res) => {
+            cb(res);
+        });
+    },
+    insertOne(column, value, cb) {
+        orm.insertOne("burgers", column, value, (res) => {
+            cb(res);
+        });
+    },
+    updateOne(column, value, cb) {
+        orm.updateOne("burgers", column, value, (res) => {
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger
