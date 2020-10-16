ce16b6689d135985ff537b1f0c7cd1ed9050c9ea
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-15 22:36:09 -0500

    updated code with cb parameter

--- config/orm.js
+++ config/orm.js
@@ -4,36 +4,36 @@ const connection = require("../config/connection.js");
 // Object Relational Mapper (ORM)
 const orm = {
     // selectAll()
-    selectAll: (table) => {
+    selectAll: (table, cb) => {
         const queryString = "SELECT * FROM ??;";
         console.log(queryString);
         const query = connection.query(queryString, [table], (err, res) => {
             if (err) {
                 throw err;
             }
-            console.log(res);
+            cb(res);
         });
     },
     // insertOne()
-    insertOne: (table, column, value) => {
+    insertOne: (table, column, value, cb) => {
         const queryString = "INSERT INTO ?? (??) VALUES (?);";
         console.log(queryString);
         const query = connection.query(queryString, [table, column, value], (err, res) => {
             if (err) {
                 throw err;
             }
-            console.log(res);
+            cb(res);
         });
     },
     // updateOne()
-    updateOne: (table, column, value) => {
+    updateOne: (table, column, value, cb) => {
         const queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?;";
         console.log(queryString);
         const query = connection.query(queryString, [table, column, value, column, value], (err, res) => {
             if (err) {
                 throw err;
             }
-            console.log(res);
+            cb(res);
         });
     }
 }
