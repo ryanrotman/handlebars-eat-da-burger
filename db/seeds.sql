36741bd5a70add7383efd1dbe195cb8e46f73729
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-15 21:44:09 -0500

    added additional seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Banzai Burger");
 INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Bacon Cheeseburger", true);
