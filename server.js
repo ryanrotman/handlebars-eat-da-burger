bbc0e5a0a6a30708279c955524e45be360894499
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-16 07:45:20 -0500

    added server.js code

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Middleware:
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is live @ http://localhost:${PORT}`);
+});
