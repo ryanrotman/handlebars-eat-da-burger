77942a2e9558116b10b84c95ee14b0694e047d89
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-16 07:35:14 -0500

    added comments/TODOs

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
+// READ
 router.get("/", (req, res) => {
     burger.selectAll((data) => {
         const viewData = {
@@ -16,12 +17,14 @@ router.get("/", (req, res) => {
     });
 });
 
+// CREATE
 router.post("/api/burgers", (req, res) => {
     burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) => {
         res.json({ id: result.insertId });
     });
 });
 
+// UPDATE
 router.put("/api/burgers/:id", (req, res) => {
     const condition = { id: req.params.id };
     console.log("condition: ", condition)
@@ -32,6 +35,8 @@ router.put("/api/burgers/:id", (req, res) => {
         res.status(200).end();
     });
 });
+
+// DELETE TODO:
 
 // Export routes for server.js to use.
 module.exports = router;
