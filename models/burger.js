915c055b1c04df1b5e29f9572a4c2d19574ba715
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-18 07:43:20 -0500

    added code for deleteOne

--- models/burger.js
+++ models/burger.js
@@ -2,21 +2,25 @@ const orm = require("../config/orm.js");
 
 const burger = {
     selectAll(cb) {
-        orm.selectAll("burgers", (res) => {
-            cb(res);
+        orm.selectAll("burgers", (results) => {
+            cb(results);
         });
     },
     insertOne(column, value, cb) {
-        orm.insertOne("burgers", column, value, (res) => {
-            cb(res);
+        orm.insertOne("burgers", column, value, (result) => {
+            cb(result);
         });
     },
     updateOne(column, value, cb) {
-        orm.updateOne("burgers", column, value, (res) => {
-            cb(res);
+        orm.updateOne("burgers", column, value, (result) => {
+            cb(result);
+        });
+    },
+    deleteOne(condition, cb) {
+        orm.deleteOne("burgers", condition, (result) => {
+            cb(result);
         });
     }
-    // TODO: deleteOne()
 }
 
 module.exports = burger
