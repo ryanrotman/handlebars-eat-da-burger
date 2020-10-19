ac1478d81dca107fb4dbc848c30edd6853767e4c
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-19 12:32:49 -0500

    added additional notes in the console logs

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
         const viewData = {
             burgers: data
         };
-        console.log(viewData);
+        console.log("GET from burgers_controller.js:", viewData);
         res.render("index", viewData);
     });
 });
@@ -27,7 +27,7 @@ router.post("/api/burgers", (req, res) => {
 // UPDATE
 router.put("/api/burgers/:id", (req, res) => {
     const condition = { id: req.params.id };
-    console.log("condition: ", condition)
+    console.log("PUT from burgers_controller.js: condition:", condition)
     burger.updateOne({ devoured: req.body.devoured }, condition, (result) => {
         if (result.changedRows == 0) {
             return res.status(404).end();
@@ -39,7 +39,7 @@ router.put("/api/burgers/:id", (req, res) => {
 // DELETE
 router.delete("/api/burgers/:id", (req, res) => {
     const condition = { id: req.params.id };
-    console.log("condition:", condition);
+    console.log("DELETE from burgers_controller.js: condition:", condition);
     burger.deleteOne(condition, (result) => {
         if (result.affectedRow == 0) {
             return res.status(404).end();
