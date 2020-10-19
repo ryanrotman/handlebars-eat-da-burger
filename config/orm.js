6ad7092b84c6c1a5580b1dc02777ff5109f8778f
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-19 12:08:26 -0500

    updated query code

--- config/orm.js
+++ config/orm.js
@@ -1,52 +1,90 @@
 // Import MySQL connection.
 const connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    let arr = [];
+    for (let i = 0; i > num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(obj) {
+    let arr = [];
+    for (let key in obj) {
+        let value = obj[key];
+        if (Object.hasOwnProperty.call(obj, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = `${value}`;
+            }
+            arr.push(`${key}=${value}`);
+        }
+    }
+    return arr.toString();
+}
+
 // Object Relational Mapper (ORM)
 const orm = {
     // selectAll()
-    selectAll: (table, cb) => {
-        const queryString = "SELECT * FROM ??;";
+    selectAll: (tableInput, cb) => {
+        const queryString = `SELECT * FROM ${tableInput};`;
         console.log(queryString);
-        const query = connection.query(queryString, [table], (err, res) => {
+        connection.query(queryString, (err, result) => {
             if (err) {
                 throw err;
             }
-            cb(res);
+            cb(result);
         });
     },
     // insertOne()
-    insertOne: (table, column, value, cb) => {
-        const queryString = "INSERT INTO ?? (??) VALUES (?);";
+    insertOne: (table, cols, vals, cb) => {
+        let queryString = `INSERT INTO ${table}`;
+        
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
         console.log(queryString);
-        const query = connection.query(queryString, [table, column, value], (err, res) => {
+
+        connection.query(queryString, vals, (err, result) => {
             if (err) {
                 throw err;
             }
-            cb(res);
+            cb(result);
         });
     },
     // updateOne()
-    updateOne: (table, column, value, cb) => {
-        const queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?;";
+    updateOne: (table, objColVals, condition, cb) => {
+        let queryString = `UPDATE ${table}`;
+        
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
         console.log(queryString);
-        const query = connection.query(queryString, [table, column, value, column, value], (err, res) => {
+
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    deleteOne: (table, condition, cb) => {
+        const queryString = `DELETE FROM ${table} WHERE ${condition};`;
+        console.log(queryString);
+        connection.query(queryString, (err, res) => {
             if (err) {
                 throw err;
             }
             cb(res);
         });
-    },
-    deleteOne: (table, condition, cb) => {
-        const queryString = "DELETE FROM ?? WHERE ??;";
-        console.log(queryString);
-        const query = connection.query(queryString, [table, condition], (err, res) => {
-            if (err) {
-                throw err;
-            }
-            cb(res);
-        })
     }
-}
+};
 
 // Export the orm object so our models can use it!
 module.exports = orm;
