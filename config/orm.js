fa8e104aac92b029649fd17fedf745f9078f862e
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-20 01:31:56 -0500

    changed for loop code in printQuestionMarks(); > to <

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ const connection = require("../config/connection.js");
 
 function printQuestionMarks(num) {
     let arr = [];
-    for (let i = 0; i > num; i++) {
+    for (let i = 0; i < num; i++) {
         arr.push("?");
     }
     return arr.toString();
